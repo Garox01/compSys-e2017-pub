082021d0def6cbacbad1e9f26023c4d431405ba6
Michael Kirkedal Thomsen <kirkedal@acm.org>
2017-09-06 09:02:17 +0200

    First files

---
+++ lectures/170906/logic.c
@@ -0,0 +1,18 @@
+//
+// Author:  Michael Kirkedal Thomsen
+//
+// Fibonacci function
+//
+// Made for CompSys17 C, lecture 2
+//
+
+#include <stdio.h>
+
+int main() {
+  int x = 224345234;
+  // int y = -1;
+
+  printf ("Result is: %X\n", x);
+
+}
+
